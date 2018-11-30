7171b84bb8fa48ff674914430cf433491968b448
hank <>
2018-11-30 18:07:44 +0800

    copy code

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# tokenlib
+# Tokenlib
+
+Golang port of [tokenlib](https://github.com/mozilla-services/tokenlib).
+
+## Examples
+
+```go
+import (
+	"fmt"
+	"time"
+
+	"github.com/youyaochi/tokenlib"
+)
+
+func main() {
+	secret := "I_LIKE_UNICORNS"
+	data := map[string]interface{}{
+		"Id":   1,
+		"Name": "One",
+	}
+	fmt.Println("Raw data:", data)
+
+	token, err := tokenlib.MakeToken(data, secret, 1)
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		return
+	}
+	fmt.Println("Token:", token)
+
+	newData, err := tokenlib.ParseToken(token, secret)
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		return
+	}
+	fmt.Println("Parsed Data:", newData)
+
+	time.Sleep(2 * time.Second)
+	expiredData, err := tokenlib.ParseToken(token, secret)
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		return
+	}
+	fmt.Println("Expired data:", expiredData)
+}
+```
---
+++ tokenlib.go
@@ -0,0 +1,112 @@
+package tokenlib
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+
+	"golang.org/x/crypto/hkdf"
+)
+
+func toInt64(v interface{}) (int64, error) {
+	switch i := v.(type) {
+	case int:
+		return int64(i), nil
+	case int64:
+		return int64(i), nil
+	case float32:
+		return int64(i), nil
+	case float64:
+		return int64(i), nil
+	default:
+		return 0, errors.New("Not Number.")
+	}
+}
+
+func signSecret(secret string, len int) []byte {
+	hash := sha256.New
+	info := []byte("services.mozilla.com/tokenlib/v1/signing")
+	hkdf := hkdf.New(hash, []byte(secret), nil, info)
+	result := make([]byte, len)
+	hkdf.Read(result)
+	return result
+}
+
+func generateSig(secret, payload []byte) []byte {
+	mac := hmac.New(sha256.New, secret)
+	mac.Write(payload)
+	sig := mac.Sum(nil)
+	return sig
+}
+
+func MakeToken(v map[string]interface{}, secret string, timeout int64) (string, error) {
+	if _, ok := v["salt"].(string); !ok {
+		salt := make([]byte, 3)
+		rand.Read(salt)
+		dstSalt := make([]byte, hex.EncodedLen(3))
+		hex.Encode(dstSalt, salt)
+		v["salt"] = dstSalt
+	}
+
+	if _, ok := v["expires"].(int); !ok {
+		expires := int64(time.Now().Unix()) + timeout
+		v["expires"] = expires
+	}
+
+	payload, err := json.Marshal(v)
+	if err != nil {
+		fmt.Println("error:", err)
+		return "", err
+	}
+
+	sig := generateSig(signSecret(secret, sha256.Size), payload)
+	raw := append(payload, sig[:]...)
+	token := base64.URLEncoding.EncodeToString(raw)
+
+	return token, nil
+}
+
+func ParseToken(token string, secret string) (map[string]interface{}, error) {
+	decodedToken, err := base64.URLEncoding.DecodeString(token)
+	if err != nil {
+		return nil, err
+	}
+	sig := decodedToken[len(decodedToken)-sha256.Size:]
+	payload := decodedToken[:len(decodedToken)-sha256.Size]
+	realSig := generateSig(signSecret(secret, sha256.Size), payload)
+
+	if !bytes.Equal(sig, realSig) {
+		return nil, errors.New("Invalid signiture.")
+	}
+
+	var v interface{}
+	err = json.Unmarshal(payload, &v)
+	if err != nil {
+		return nil, err
+	}
+
+	data, ok := v.(map[string]interface{})
+	if !ok {
+		return nil, errors.New("Invalid json.")
+	}
+
+	expires, err := toInt64(data["expires"])
+	if err != nil {
+		return nil, errors.New("Invalid json.")
+	}
+	if expires < time.Now().Unix() {
+		return nil, errors.New("Token has expired.")
+	}
+
+	delete(data, "salt")
+	delete(data, "expires")
+
+	return data, nil
+}
